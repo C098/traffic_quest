4f7e0044cb98d5d64b8f49217c82f00fd3c86708
Will Mayer <william.mayer@outlook.com>
2022-06-08 10:36:26 +0400

    seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require "open-uri"
+require "nokogiri"
+
+url = 'https://en.wikipedia.org/wiki/List_of_places_in_Mauritius'
+
+html_file = URI.open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+ar = []
+html_doc.search(".new").first(10).each do |element|
+  # puts element.text.strip
+  ar << element.text.strip
+end
+
 puts 'cleaning db'
+
 User.destroy_all
 Event.destroy_all
 
 puts 'Creating users...'
 
-user1 = User.create(email: 'sam@gmail.com', password: "1234567")
+arcat = ['crash', 'traffic jam', 'road closed', 'police check', 'speed gun', 'booze truck']
+5.times do
+  user1 = User.create(
+    email: Faker::Internet.email,
+    password: '123456'
+  )
+  event1 = Event.create(
+    category: arcat.sample,
+    address: "#{ar.sample}, Mauritius",
+    user_id: user1.id
+  )
+end
 
 puts 'creating events'
-
-Event.create!(user: user1, address: "Port-Louis, Mauritius", category: 'crash')
-Event.create!(user: user1, address: "Grand Baie, Mauritius", category: 'crash')
 
 puts 'finished'
