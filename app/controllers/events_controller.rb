4b95cdc257072d5e7ca4d3ccfb09361b376dbeeb
hemamomple <hmomple93@gmail.com>
2022-06-10 15:21:48 +0400

    update controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -31,7 +31,8 @@ class EventsController < ApplicationController
 
   def destroy
     @event = Event.find(params[:id])
-    @event.destroy(event_params)
+    @event.destroy
+    redirect_to events_path
   end
 
   def show
