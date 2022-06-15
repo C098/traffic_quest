c0a563464c4c4911d16ccde862375b55d97f4e6f
Mohammad Chady <cma37@outlook.com>
2022-06-15 10:46:05 +0400

    errorpage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,7 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+        // Parallax Code
+        var scene = document.getElementById('scene');
+        var parallax = new Parallax(scene);
